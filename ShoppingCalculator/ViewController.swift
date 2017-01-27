42f6b18dba478532e62a343e9bd546a1782cc70e
Glizela Taino <glizelataino@yahoo.com>
2017-01-27 11:34:22 -0600

    Functional

--- ShoppingCalculator/ViewController.swift
+++ ShoppingCalculator/ViewController.swift
@@ -9,10 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var originalPriceTextField: UITextField!
+    @IBOutlet weak var finalPriceLabel: UILabel!
+    @IBOutlet weak var discountPriceTextField: UITextField!
+    @IBOutlet weak var taxTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    @IBAction func textChanged(_ sender: UITextField) {
+        var originalPriceValue = "0"
+        var discountValue = "0"
+        var taxValue = "0"
+        
+        if let price = originalPriceTextField.text, isValid(input: price){
+            originalPriceValue = price
+            if let discount = discountPriceTextField.text, isValid(input: discount){
+                discountValue = discount
+                if let tax = taxTextField.text, isValid(input: tax){
+                    taxValue = tax
+                    
+                }
+            }
+        }
+        
+        let finalPrice = calculate(originalPrice: Double(originalPriceValue)!, discount: Double(discountValue)!, tax: Double(taxValue)!)
+        
+        finalPriceLabel.text = "\(finalPrice)"
+        
+    }
+    
+    func isValid(input: String) -> Bool {
+        //empty, zero (who cares if it's zero), negative, not numbers
+        
+        let inputValue = Double(input)
+        if inputValue == nil{
+            return false
+        }
+        
+        return true
+    }
+    
+    
+    func calculate(originalPrice: Double, discount: Double, tax: Double) -> Double {
+        
+        var finalPrice = originalPrice - ((discount/100) * originalPrice)
+        finalPrice = originalPrice + ((tax/100) * originalPrice)
+        
+        return finalPrice
     }
 
     override func didReceiveMemoryWarning() {
